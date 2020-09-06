a3266b019b3ba978f692a53c9ae1dc0cacf7a7cd
Andre Moreira <andre.moreira@focus-sys.com.br>
2020-09-06 19:02:56 -0300

    cors adjusted

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://grades-app-andrem.herokuapp.com/',
   })
 );
 
